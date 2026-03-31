612664d6303344f9f746b7c51140489e421648b3
meet2960 <meetghelani43@gmail.com>
2026-03-31 19:15:19 +0530

    fix: remove import.meta stuff as we are not using type as module

--- src/common/utility/paths.ts
+++
@@ -1,9 +0,0 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// assuming this file lives under src/utils
-export const DIST_PROJECT_ROOT = path.resolve(__dirname, '../../'); // dist
-export const TOP_LEVEL_PROJECT_ROOT = path.resolve(__dirname, '../../../'); // top level project directory

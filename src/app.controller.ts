94f3bdadbf1878ee366abce513385546c349b618
meet2960 <meetghelani43@gmail.com>
2026-04-01 01:31:44 +0530

    add: demo prisma query usage

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('users')
+  getAllUsers() {
+    return this.appService.getAllUsers();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismadbService } from './modules/helpers/prismadb/prismadb.service';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismadbService) {}
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!!';
+  }
+
+  async getAllUsers() {
+    const res = await this.prisma.roles.findMany({
+      select: {
+        role_id: true,
+        role_name: true,
+      },
+    });
+    return res;
   }
 }

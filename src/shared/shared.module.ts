86ae6c971c7f19b9ec35a52600555443b63e733f
meet2960 <meetghelani43@gmail.com>
2026-03-31 20:33:05 +0530

    fix: shared module

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -3,13 +3,8 @@ import { Global, Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ApiConfigService } from './services/api-config.service';
 import { AwsS3Service } from './services/aws-s3.service';
-import { TranslationService } from './services/translation.service';
 
-const providers: Provider[] = [
-  ApiConfigService,
-  AwsS3Service,
-  TranslationService,
-];
+const providers: Provider[] = [ApiConfigService, AwsS3Service];
 
 @Global()
 @Module({

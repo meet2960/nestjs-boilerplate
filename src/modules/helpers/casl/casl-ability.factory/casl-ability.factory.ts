a2ddd8985cbe13b3bce7369f9473e7c6c478ea2a
meet2960 <meetghelani43@gmail.com>
2026-08-25 01:46:26 +0530

    refactor: casl ability module with some proper code practices

--- src/modules/helpers/casl/casl-ability.factory/casl-ability.factory.ts
+++ src/modules/helpers/casl/casl-ability.factory/casl-ability.factory.ts
@@ -1,15 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { flatMap } from 'lodash-es';
+import { ActionCode } from '@/common/constants/action-code.constants';
+import { PageCode } from '@/common/constants/page-code.constants';
+import { Actions, AppAbility, Subjects } from '@/common/types/casl.interface';
+import { IPagePermissions } from '@/common/types/jwt-payload.interface';
 import type { IUserSession } from '@/common/types/user-session.interface';
 import { AbilityBuilder, createMongoAbility } from '@casl/ability';
 import type { ExtractSubjectType } from '@casl/ability';
-import {
-  Action,
-  type AppAbility,
-  type IPagePermissions,
-  type Subjects,
-} from '../static/casl.types';
-import { PageCode } from '../static/page-code';
 
 @Injectable()
 export class CaslAbilityFactory {
@@ -19,14 +16,14 @@ export class CaslAbilityFactory {
   ): AppAbility {
     const { can, build } = new AbilityBuilder<AppAbility>(createMongoAbility);
     if (user.role_code === 'SUPER_ADMIN') {
-      can(Action.Manage, PageCode.all);
+      can(ActionCode.manage, PageCode.all);
     } else {
       const transformedPermission = flatMap(
         permissions,
         ({ action_list, page_code }) =>
           action_list.map((action) => ({
             name: page_code.toLocaleLowerCase() as Subjects,
-            value: action.toLocaleLowerCase() as Action,
+            value: action.toLocaleLowerCase() as Actions,
           })),
       );
       transformedPermission.forEach((item, _index) =>
@@ -34,6 +31,15 @@ export class CaslAbilityFactory {
       );
     }
 
+    return build({
+      detectSubjectType: (item: any) =>
+        item.constructor as ExtractSubjectType<Subjects>,
+    });
+  }
+
+  buildPermissionForAdminUsers(): AppAbility {
+    const { can, build } = new AbilityBuilder<AppAbility>(createMongoAbility);
+    can(ActionCode.manage, PageCode.all);
     return build({
       detectSubjectType: (item: any) =>
         item.constructor as ExtractSubjectType<Subjects>,
